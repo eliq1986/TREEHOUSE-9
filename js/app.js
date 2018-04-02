2407a56e6ae29ee1bc43042814fd7887e275e099
Elijah Quesada <this_boy10@yahoo.com>
2018-04-02 14:41:35 -0700

    First chart
    -First chart completed

---
+++ js/app.js
@@ -0,0 +1,81 @@
+const CHART = document.getElementById("lineChart").getContext("2d");
+const barChart = document.getElementById("barChart").getContext("2d");
+const pieChart = document.getElementById("pieChart").getContext("2d");
+
+
+let lineChart = new Chart(CHART, {
+  type: "line",
+  data: {
+        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+        datasets: [{
+            label: '# of Votes',
+            data: [500, 200, 1000, 1800, 2500, 2000, 400, 1500, 300, 800, 1200, 2000],
+            backgroundColor: [
+                'rgba(214, 206, 224, 0.4)'
+            ],
+            borderColor: [
+                "rgb(139, 153, 232)"
+            ],
+            borderWidth: 2
+        }]
+    },
+
+});
+
+let bChart= new Chart(barChart, {
+  type: "bar",
+  data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+
+});
+
+let pChart= new Chart(pieChart, {
+  type: "pie",
+  data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+
+});
