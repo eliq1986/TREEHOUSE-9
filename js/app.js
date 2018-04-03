b5fd613fe7388cb764e5d1eaed518022afda0deb
Elijah Quesada <this_boy10@yahoo.com>
2018-04-02 19:00:29 -0700

    First Half Mobile
    First Half Of mobile build completed

--- js/app.js
+++ js/app.js
@@ -19,63 +19,74 @@ let lineChart = new Chart(CHART, {
             borderWidth: 2
         }]
     },
+   options: {
+       title: {
+        display: true,
+        text: "Traffic",
+        fontSize: 20,
+        position: "top"
+
+      }
+
+   }
 
 });
 
 let bChart= new Chart(barChart, {
   type: "bar",
   data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [50, 100, 180, 120, 230, 200],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
+                "#8b99e8",
+                "#8b99e8",
+                "#8b99e8",
+                "#8b99e8",
+                "#8b99e8",
+                "#8b99e8",
+          ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+
             ],
             borderWidth: 1
         }]
     },
+    options: {
+        title: {
+         display: true,
+         text: "Daily Traffic",
+         fontSize: 20,
+         position: "top"
 
+       }
+}
 });
 
 let pChart= new Chart(pieChart, {
-  type: "pie",
+  type: "doughnut",
   data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Phones", "Tablet", "Desktop",],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                "#8b99e8",
+                "#00aced",
+                "#3b5998"
+
             ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+          borderWidth: 0
         }]
     },
+    options: {
+        title: {
+         display: true,
+         text: "Mobile User",
+         fontSize: 20,
+         position: "top"
 
+       }
+}
 });
